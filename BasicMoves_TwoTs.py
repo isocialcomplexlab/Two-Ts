d4446d6cc8a7f84dc4ea7a7947195e412ae1ed38
Tamires dos Santos <tamires.04.santos@gmail.com>
2022-01-12 10:06:40 -0300

    Update BasicMoves_TwoTs.py

--- BasicMoves_TwoTs.py
+++ BasicMoves_TwoTs.py
@@ -3,16 +3,16 @@
 
 #######################################################
 #                      TWO-TS
-# Trabalho de Mestrado em Ciência da Computação da Universidade Federal do ABC (UFABC);
-# Título do Trabalho: Two-Ts: Reprodução de Expressões Faciais em Cabeça Robótica com Tecnologia 3D;
-# Discente: Tamires dos Santos;
-# Orientador: Wagner Tanaka Botelho;
+#  Master's degree in Computer Science at the Federal University of ABC;
+#  Title of Work: Two-Ts: Reproduction of Facial Expressions in Robotic Head with 3D Technology;
+#  Student: Tamires dos Santos;
+#  Advisor: Wagner Tanaka Botelho;
 #
-# Descrição: Movimentações básica da TWO-TS. 
+#  Description: Basic movements of the Two-Ts.
 #
-# Princiapis referências: https://github.com/adafruit/Adafruit_Python_PCA9685/blob/master/examples/simpletest.py
-# 
-# Data de Modificação: 12/05/2021
+#  Main references: https://github.com/adafruit/Adafruit_Python_PCA9685/blob/master/examples/simpletest.py
+#
+#  Modification Date: 05/12/2021
 #######################################################
 
 from __future__ import division
@@ -22,228 +22,228 @@ import Adafruit_PCA9685
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
-#### Servomotores/Posições
-# Pescoço
+#### Servomotors/Positions
+# Neck
 m0 = 0
 m1 = 1
 m2 = 2
-# Maxilar
+# Jaw
 m3 = 3
 # Boca
 m4 = 4
 m5 = 5
-# Sobrancelhas
+# Eyebrows
 m6 = 6
 m7 = 7
-# Olhos
-m8 = 8 #esquerdo
-m9 = 9 #direito
-# Palpebras
-m10 = 10 #esquerda
-m11 = 11 #direita
-# Boca (triste)
-m12 = 12 #esquerda
-m13 = 13 #direita
+# Eyes
+m8 = 8 #left
+m9 = 9 #right
+# Eyelids
+m10 = 10 #left
+m11 = 11 #right
+# Mouth
+m12 = 12 #left
+m13 = 13 #right
 #################
 
-# Comprimento do Pulso 
-minPulso = 650
-maxPulso = 2350
+# Wrist length
+minPulse = 650
+maxPulse = 2350
 
-def definirAngulo(angulo):
-    larguraPulso = 0
-    valorAnalogico = 0
-    larguraPulso = (angulo - 0)*(maxPulso-minPulso)/180 +minPulso
-    valorAnalogico = int(float(larguraPulso) / 1000000 * 50 * 4096)
-    return valorAnalogico
+def setAngle(angle):
+    pulseWidth = 0
+    analogValue = 0
+    pulseWidth = (angle - 0)*(maxPulse-minPulse)/180 +minPulse
+    analogValue = int(float(pulseWidth) / 1000000 * 50 * 4096)
+    return analogValue
 
 
-# Posicao inicial
-pwm.set_pwm(m0, 0, definirAngulo(100))
-pwm.set_pwm(m1, 0, definirAngulo(50))
-pwm.set_pwm(m2, 0, definirAngulo(70))
-pwm.set_pwm(m3, 0, definirAngulo(0))
-pwm.set_pwm(m4, 0, definirAngulo(100))
-pwm.set_pwm(m5, 0, definirAngulo(90))
-pwm.set_pwm(m6, 0, definirAngulo(130))
-pwm.set_pwm(m7, 0, definirAngulo(50))
-pwm.set_pwm(m8, 0, definirAngulo(90))
-pwm.set_pwm(m9, 0, definirAngulo(90))
-pwm.set_pwm(m10, 0, definirAngulo(20))
-pwm.set_pwm(m11, 0, definirAngulo(120))
-pwm.set_pwm(m12, 0, definirAngulo(90))
-pwm.set_pwm(m13, 0, definirAngulo(180))
+# Starting position
+pwm.set_pwm(m0, 0, setAngle(100))
+pwm.set_pwm(m1, 0, setAngle(50))
+pwm.set_pwm(m2, 0, setAngle(70))
+pwm.set_pwm(m3, 0, setAngle(0))
+pwm.set_pwm(m4, 0, setAngle(100))
+pwm.set_pwm(m5, 0, setAngle(90))
+pwm.set_pwm(m6, 0, setAngle(130))
+pwm.set_pwm(m7, 0, setAngle(50))
+pwm.set_pwm(m8, 0, setAngle(90))
+pwm.set_pwm(m9, 0, setAngle(90))
+pwm.set_pwm(m10, 0, setAngle(20))
+pwm.set_pwm(m11, 0, setAngle(120))
+pwm.set_pwm(m12, 0, setAngle(90))
+pwm.set_pwm(m13, 0, setAngle(180))
 
 
 while True:
-    print("1 - Cima")
-    print("2 - Baixo")
-    print("3 - Esquerda")
-    print("4 - Direita")
-    print("5 - Rotação para direita")
-    print("6 - Rotação para esquerda")
-    print("7 - Maxilar")
-    print("8 - Sorriso")
-    print("9 - Sobrancelha esquerda")
-    print("10 - Sobrancelha direita")
-    print("11 - Olhos para esquerda")
-    print("12 - Olhos para direita")
-    print("13 - Piscar olho esquerdo")
-    print("14 - Piscar olho direito")
-    print("15 - Sorriso triste")
+    print("1 - Head up")
+    print("2 - Head down")
+    print("3 - head to left")
+    print("4 - head to right")
+    print("5 - Head rotation to the right")
+    print("6 - Head rotation to the left")
+    print("7 - Jaw")
+    print("8 - Smile")
+    print("9 - Left eyebrow")
+    print("10 - Right eyebrow")
+    print("11 - Eyes to the left")
+    print("12 - Eyes to the right")
+    print("13 - Blink the left eye")
+    print("14 - Blink the right eye")
+    print("15 - Smile down")
     
-    opcao = input()
+    option = input()
     time.sleep(2)
 
-    if opcao == '1':
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
+    if option == '1':
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(10))
-        pwm.set_pwm(m2, 0, definirAngulo(120))
+        pwm.set_pwm(m1, 0, setAngle(10))
+        pwm.set_pwm(m2, 0, setAngle(120))
         time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
-        time.sleep(1)
-
-    elif opcao == '2':
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
-        time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(90))
-        pwm.set_pwm(m2, 0, definirAngulo(20))
-        time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
 
-    elif opcao == '3':
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
+    elif option == '2':
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(10))
-        pwm.set_pwm(m2, 0, definirAngulo(20))
+        pwm.set_pwm(m1, 0, setAngle(90))
+        pwm.set_pwm(m2, 0, setAngle(20))
         time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
-        time.sleep(1)
-
-    elif opcao == '4':
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
-        time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(100))
-        pwm.set_pwm(m2, 0, definirAngulo(120))
-        time.sleep(1)
-        pwm.set_pwm(m1, 0, definirAngulo(50))
-        pwm.set_pwm(m2, 0, definirAngulo(70))
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
 
-    elif opcao == '5':
-        pwm.set_pwm(m0, 0, definirAngulo(100))
+    elif option == '3':
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m0, 0, definirAngulo(70))
+        pwm.set_pwm(m1, 0, setAngle(10))
+        pwm.set_pwm(m2, 0, setAngle(20))
         time.sleep(1)
-        pwm.set_pwm(m0, 0, definirAngulo(100))
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
 
-    elif opcao == '6':
-        pwm.set_pwm(m0, 0, definirAngulo(100))
+    elif option == '4':
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m0, 0, definirAngulo(130))
+        pwm.set_pwm(m1, 0, setAngle(100))
+        pwm.set_pwm(m2, 0, setAngle(120))
         time.sleep(1)
-        pwm.set_pwm(m0, 0, definirAngulo(100))
+        pwm.set_pwm(m1, 0, setAngle(50))
+        pwm.set_pwm(m2, 0, setAngle(70))
         time.sleep(1)
-        
-    elif opcao == '7':
-        pwm.set_pwm(m3, 0, definirAngulo(0))
+
+    elif option == '5':
+        pwm.set_pwm(m0, 0, setAngle(100))
         time.sleep(1)
-        pwm.set_pwm(m3, 0, definirAngulo(90))
+        pwm.set_pwm(m0, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m3, 0, definirAngulo(0))
+        pwm.set_pwm(m0, 0, setAngle(100))
         time.sleep(1)
-        
-    elif opcao == '8':
-        pwm.set_pwm(m4, 0, definirAngulo(100))
-        pwm.set_pwm(m5, 0, definirAngulo(90))
+
+    elif option == '6':
+        pwm.set_pwm(m0, 0, setAngle(100))
         time.sleep(1)
-        pwm.set_pwm(m4, 0, definirAngulo(10))
-        pwm.set_pwm(m5, 0, definirAngulo(180))
+        pwm.set_pwm(m0, 0, setAngle(130))
         time.sleep(1)
-        pwm.set_pwm(m4, 0, definirAngulo(100))
-        pwm.set_pwm(m5, 0, definirAngulo(90))
-        time.sleep(1)
-        
-    elif opcao == '9':
-        pwm.set_pwm(m6, 0, definirAngulo(130))
-        time.sleep(1)
-        pwm.set_pwm(m6, 0, definirAngulo(110))
-        time.sleep(1)
-        pwm.set_pwm(m6, 0, definirAngulo(130))
-        time.sleep(1)
-        pwm.set_pwm(m6, 0, definirAngulo(150))
-        time.sleep(1)
-        pwm.set_pwm(m6, 0, definirAngulo(130))
+        pwm.set_pwm(m0, 0, setAngle(100))
         time.sleep(1)
         
-    elif opcao == '10':
-        pwm.set_pwm(m7, 0, definirAngulo(50))
+    elif option == '7':
+        pwm.set_pwm(m3, 0, setAngle(0))
         time.sleep(1)
-        pwm.set_pwm(m7, 0, definirAngulo(80))
+        pwm.set_pwm(m3, 0, setAngle(90))
         time.sleep(1)
-        pwm.set_pwm(m7, 0, definirAngulo(50))
-        time.sleep(1)
-        pwm.set_pwm(m7, 0, definirAngulo(20))
-        time.sleep(1)
-        pwm.set_pwm(m7, 0, definirAngulo(50))
+        pwm.set_pwm(m3, 0, setAngle(0))
         time.sleep(1)
         
-    elif opcao == '11':
-        pwm.set_pwm(m8, 0, definirAngulo(90))
-        pwm.set_pwm(m9, 0, definirAngulo(90))
+    elif option == '8':
+        pwm.set_pwm(m4, 0, setAngle(100))
+        pwm.set_pwm(m5, 0, setAngle(90))
         time.sleep(1)
-        pwm.set_pwm(m8, 0, definirAngulo(60))
-        pwm.set_pwm(m9, 0, definirAngulo(60))
+        pwm.set_pwm(m4, 0, setAngle(10))
+        pwm.set_pwm(m5, 0, setAngle(180))
         time.sleep(1)
-        pwm.set_pwm(m8, 0, definirAngulo(90))
-        pwm.set_pwm(m9, 0, definirAngulo(90))
-        time.sleep(1)
-        
-    elif opcao == '12':
-        pwm.set_pwm(m8, 0, definirAngulo(90))
-        pwm.set_pwm(m9, 0, definirAngulo(90))
-        time.sleep(1)
-        pwm.set_pwm(m8, 0, definirAngulo(120))
-        pwm.set_pwm(m9, 0, definirAngulo(120))
-        time.sleep(1)
-        pwm.set_pwm(m8, 0, definirAngulo(90))
-        pwm.set_pwm(m9, 0, definirAngulo(90))
+        pwm.set_pwm(m4, 0, setAngle(100))
+        pwm.set_pwm(m5, 0, setAngle(90))
         time.sleep(1)
         
-    elif opcao == '13':
-        pwm.set_pwm(m10, 0, definirAngulo(20))
+    elif option == '9':
+        pwm.set_pwm(m6, 0, setAngle(130))
         time.sleep(1)
-        pwm.set_pwm(m10, 0, definirAngulo(100))
+        pwm.set_pwm(m6, 0, setAngle(110))
         time.sleep(1)
-        pwm.set_pwm(m10, 0, definirAngulo(20))
+        pwm.set_pwm(m6, 0, setAngle(130))
+        time.sleep(1)
+        pwm.set_pwm(m6, 0, setAngle(150))
+        time.sleep(1)
+        pwm.set_pwm(m6, 0, setAngle(130))
+        time.sleep(1)
+        
+    elif option == '10':
+        pwm.set_pwm(m7, 0, setAngle(50))
+        time.sleep(1)
+        pwm.set_pwm(m7, 0, setAngle(80))
+        time.sleep(1)
+        pwm.set_pwm(m7, 0, setAngle(50))
+        time.sleep(1)
+        pwm.set_pwm(m7, 0, setAngle(20))
+        time.sleep(1)
+        pwm.set_pwm(m7, 0, setAngle(50))
+        time.sleep(1)
+        
+    elif option == '11':
+        pwm.set_pwm(m8, 0, setAngle(90))
+        pwm.set_pwm(m9, 0, setAngle(90))
+        time.sleep(1)
+        pwm.set_pwm(m8, 0, setAngle(60))
+        pwm.set_pwm(m9, 0, setAngle(60))
+        time.sleep(1)
+        pwm.set_pwm(m8, 0, setAngle(90))
+        pwm.set_pwm(m9, 0, setAngle(90))
+        time.sleep(1)
+        
+    elif option == '12':
+        pwm.set_pwm(m8, 0, setAngle(90))
+        pwm.set_pwm(m9, 0, setAngle(90))
+        time.sleep(1)
+        pwm.set_pwm(m8, 0, setAngle(120))
+        pwm.set_pwm(m9, 0, setAngle(120))
+        time.sleep(1)
+        pwm.set_pwm(m8, 0, setAngle(90))
+        pwm.set_pwm(m9, 0, setAngle(90))
+        time.sleep(1)
+        
+    elif option == '13':
+        pwm.set_pwm(m10, 0, setAngle(20))
+        time.sleep(1)
+        pwm.set_pwm(m10, 0, setAngle(100))
+        time.sleep(1)
+        pwm.set_pwm(m10, 0, setAngle(20))
         time.sleep(1)
     
-    elif opcao == '14':
-        pwm.set_pwm(m11, 0, definirAngulo(120))
+    elif option == '14':
+        pwm.set_pwm(m11, 0, setAngle(120))
         time.sleep(1)
-        pwm.set_pwm(m11, 0, definirAngulo(70))
+        pwm.set_pwm(m11, 0, setAngle(70))
         time.sleep(1)
-        pwm.set_pwm(m11, 0, definirAngulo(120))
+        pwm.set_pwm(m11, 0, setAngle(120))
         time.sleep(1)
     
-    elif opcao == '15':
-        pwm.set_pwm(m12, 0, definirAngulo(90))
-        pwm.set_pwm(m13, 0, definirAngulo(180))
+    elif option == '15':
+        pwm.set_pwm(m12, 0, setAngle(90))
+        pwm.set_pwm(m13, 0, setAngle(180))
         time.sleep(1)
-        pwm.set_pwm(m12, 0, definirAngulo(180))
-        pwm.set_pwm(m13, 0, definirAngulo(90))
+        pwm.set_pwm(m12, 0, setAngle(180))
+        pwm.set_pwm(m13, 0, setAngle(90))
         time.sleep(1)
-        pwm.set_pwm(m12, 0, definirAngulo(90))
-        pwm.set_pwm(m13, 0, definirAngulo(180))
+        pwm.set_pwm(m12, 0, setAngle(90))
+        pwm.set_pwm(m13, 0, setAngle(180))
         time.sleep(1)
         
